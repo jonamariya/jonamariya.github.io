57ab8da646154cc8c0e465fdf2b10a4e72247979
Jona James <jonamariya@gmail.com>
2017-04-06 17:20:51 +0930

    angular App

--- cafe-app/src/public/menu-items/menu-items.controller.js
+++ cafe-app/src/public/menu-items/menu-items.controller.js
@@ -8,6 +8,13 @@ MenuItemsController.$inject = ['menuItems'];
 function MenuItemsController(menuItems) {
   var $ctrl = this;
   $ctrl.menuItems = menuItems;
+  
+  // Add 'active' to menu button if not already there
+  var classes = document.querySelector("#navMenuButton").className;
+  if (classes.indexOf("active") == -1) {
+    classes += " active";
+    document.querySelector("#navMenuButton").className = classes;
+  }
 }
 
 })();
